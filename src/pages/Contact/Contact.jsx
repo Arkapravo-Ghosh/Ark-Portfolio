4335b3b14445194011ea44acf34c1081a8b9fb55
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-25 20:42:40 +0530

    Added Background in Contact Page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Contact.css";
+import bg from "../../assets/bg.jpg";
 export default function Contact() {
   function form_submit(event) {
     event.preventDefault()
@@ -12,37 +13,40 @@ export default function Contact() {
     form.reset();
   }
   return (
-    <div className="Contact">
-      <div className="Contact-Header">
-        <h1>Contact Me</h1>
-      </div>
-      <div className="Contact-Body">
-        <div className="Contact-Body-Left">
-          <h2>Contact</h2>
-          <p>
-            <b>Email:</b>{" "}
-            <a href="mailto:arkapravoghosh99@gmail.com" target="_blank">arkapravoghosh99@gmail.com</a>
-          </p>
-          <p>
-            <b>Phone:</b>{" "}
-            <a href="tel:+917003768803" target="_blank">+91 70037 68803</a>
-          </p>
-          <p>
-            <b>WhatsApp:</b>{" "}
-            <a href="https://wa.me/917003768803" target="_blank">+91 70037 68803</a>
-          </p>
+    <>
+      <div className="bg" />
+      <div className="Contact">
+        <div className="Contact-Header">
+          <h1>Contact Me</h1>
         </div>
-        <div className="Contact-Body-Right">
-          <h2>Get in Touch</h2>
-          <form id="contact-form" onSubmit={form_submit}>
-            <input type="hidden" name="contact_number" />
-            <input type="text" placeholder="Name" name="user_name" />
-            <input type="email" placeholder="Email" name="user_email" />
-            <textarea placeholder="Message" name="message" />
-            <input class="button" type="submit" value="Send" />
-          </form>
+        <div className="Contact-Body">
+          <div className="Contact-Body-Left">
+            <h2>Contact</h2>
+            <p>
+              <b>Email:</b>{" "}
+              <a href="mailto:arkapravoghosh99@gmail.com" target="_blank">arkapravoghosh99@gmail.com</a>
+            </p>
+            <p>
+              <b>Phone:</b>{" "}
+              <a href="tel:+917003768803" target="_blank">+91 70037 68803</a>
+            </p>
+            <p>
+              <b>WhatsApp:</b>{" "}
+              <a href="https://wa.me/917003768803" target="_blank">+91 70037 68803</a>
+            </p>
+          </div>
+          <div className="Contact-Body-Right">
+            <h2>Get in Touch</h2>
+            <form id="contact-form" onSubmit={form_submit}>
+              <input type="hidden" name="contact_number" />
+              <input type="text" placeholder="Name" name="user_name" />
+              <input type="email" placeholder="Email" name="user_email" />
+              <textarea placeholder="Message" name="message" />
+              <input class="button" type="submit" value="Send" />
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
