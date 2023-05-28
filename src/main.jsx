d9a9d5066c3d13b296a7142c5192f8ebe90c0703
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-28 18:52:53 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import { AnimatePresence, motion } from "framer-motion";
 
 function Main() {
   return (
     <React.StrictMode>
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.1, ease: "easeInOut" }}
-        >
-          <App />
-        </motion.div>
-      </AnimatePresence>
+      <App />
     </React.StrictMode>
   )
 };
