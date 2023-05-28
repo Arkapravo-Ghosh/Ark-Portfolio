9a2f0676234c958ca3037ef1a69beb355aa31b97
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-28 19:00:28 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import './styles/App.css';
 // import AnimatedCursor from 'react-animated-cursor';
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
@@ -10,8 +10,8 @@ import About from './pages/About/About';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Contact from './pages/Contact/Contact';
 
-const TRACKING_ID = "G-X56MMZ2G83";
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = "G-X56MMZ2G83";
+// ReactGA.initialize(TRACKING_ID);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
