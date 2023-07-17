382a6af6ad5906a1907ad7b7d5cd74ba17534eff
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-07-17 18:38:31 +0530

    feat: added fade animation to approuter

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import propTypes from "prop-types";
 import NavLinks from "../NavBar/NavLinks";
 
 export default function AppRouter() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      {Object.values(NavLinks).map((link) => (
-        <Route key={link.to} path={link.to} element={link.element} />
-      ))}
-    </Routes>
+    <AnimatePresence>
+      <Routes location={location}>
+        {
+          Object.values(NavLinks).map((link) => (
+            <Route
+              key={link.to}
+              path={link.to}
+              element={<MotionRouteWrapper element={link.element} />}
+            />
+          ))
+        }
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+function MotionRouteWrapper({ element }) {
+  MotionRouteWrapper.propTypes = {
+    element: propTypes.element.isRequired,
+  };
+  const location = useLocation();
+
+  return (
+    <motion.div
+      key={location.pathname}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {element}
+    </motion.div>
   );
 };
