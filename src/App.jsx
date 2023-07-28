fc1a1bb7db1ae50dc46bf800f2b33b8c22afc87a
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-07-28 19:28:13 +0530

    feat: enabled google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles/App.css";
-// import ReactGA from "react-ga4";
+import ReactGA from "react-ga4";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import AppRouter from "./components/AppRouter/AppRouter";
 
-// const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
-// ReactGA.initialize(TRACKING_ID);
+const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID);
 
 export default function App() {
   return (
