9bf7be1a6dd0325558f043e2997677fa15e71df5
Arkapravo-Ghosh <arkapravoghosh99@gmail.com>
2026-04-24 17:13:44 +0530

    feat: added gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NK9HTXD2');`}
+        </Script>
         {/* JSON-LD Structured Data */}
         <script
           type="application/ld+json"
@@ -122,6 +129,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NK9HTXD2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-MTKK3M93VN"
           strategy="afterInteractive"
